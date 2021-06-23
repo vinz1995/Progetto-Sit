5a079e44d9a9a2db1bb4717bc15bd2d4f166afbf
vinz1995 <83903266+vinz1995@users.noreply.github.com>
2021-06-23 19:04:02 +0200

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,7 +1,9 @@
 <?php
 
+$db = pg_connect("host=localhost port=5432 dbname=gis2021 user=postgres password=306090120");
 
-$conn_string = "host=localhost dbname=gis2021 user=postgres password=306090120";
-$dbconn4 = pg_connect($conn_string);
-echo 'ok';
+$result = pg_query($db, "SELECT * FROM tabella_spaziale") or die('connection failed');
+$row = pg_fetch_assoc($result);
+print_r($row)
+
 ?>
