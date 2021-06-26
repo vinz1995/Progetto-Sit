91821704e6183f3becc9bcc3593541ddf0707752
vinz1995 <83903266+vinz1995@users.noreply.github.com>
2021-06-26 14:02:30 +0200

    up register

--- register.php
+++ register.php
@@ -1,55 +1,120 @@
 <?php
 session_start();
-require_once 'configDB.php';
-// Now we check if the data was submitted, isset() function will check if the data exists.
-// if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
-// 	// Could not get the data that should have been sent.
-// 	exit('Please complete the registration form!');
-// }
-// // Make sure the submitted registration values are not empty.
-// if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
-// 	// One or more values are empty.
-// 	exit('Please complete the registration form');
-// }
-
-try {
-	$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
-	// make a database connection
-	$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-	if ($pdo) {
-		echo "Connected to the $db database successfully!";
-		$stmt = $pdo->prepare("SELECT email FROM utenti WHERE email=:email");
-		$stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
-
-		$stmt->execute(); 
-		//non serve non voglio leg i res del DB
-		// $stmt->bindColumn('email', $email);// restituita
-		
-		// $user = $stmt->fetch(PDO::FETCH_ASSOC);
-		// echo $email;
-
-		
-		if ($stmt->rowCount()>0) {
-			echo 'email exists, please choose another!';
-			//header('Location: register.html');
-
-		}
-		else{
-			$stmt = $pdo->prepare("INSERT INTO utenti (email,password) VALUES (:email,:passwordInChiaro)");
-			$stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
-			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-			$stmt->bindParam(':passwordInChiaro', $password, PDO::PARAM_STR);
-			$stmt->execute(); 
-			echo 'You have successfully registered, you can now login!';
-			
-		}
-
-
-		}
-} catch (PDOException $e) {
-	die($e->getMessage());
-} finally {
-	$pdo=null;
+// If the user is not logged in redirect to the login page...
+if (isset($_SESSION['erroreEmail'])) {
+    //header('Location: index.html');
+  $erroreEmail='esiste l email';
 }
-
 ?>
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>Sticky Footer Navbar Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+<header>
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
+</header>
+
+<!-- Begin page content -->
+<main class="form-signin">
+  <div class="container">
+    <form method="POST" action="controlloReg.php">
+    <h1 class="h3 mb-3 fw-normal">Registrati</h1>
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Inserisci il nome" name="nome" required>
+      <label for="floatingInput"  >Nome</label>
+      <span class="form-control"><?php  echo $erroreEmail;?></span>
+    </div>
+    <div class="form-floating ">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Inserisci il cognome" name="cognome" required>
+      <label for="floatingInput">Cognome</label>
+    </div>
+    <div class="form-floating ">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" required>
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating ">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Inserisci il nome" name="codiceFiscale" pattern="[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]" required>
+      <label for="floatingInput">Codice Fiscale</label>
+    </div>
+     <div class="form-floating">
+      <input type="tel" class="form-control" id="floatingPassword" placeholder="10" name="telefono" pattern="[0-9]{10}" required>
+      <label for="floatingPassword">Numero di telefono</label>
+      
+    </div>
+    <div class="form-floating ">
+      <input type="password" class="form-control" id="floatingInput" placeholder="Inserisci il nome" name="password" required>
+      <label for="floatingInput">Password</label>
+    </div>
+    <div class="form-floating ">
+      <input type="password" class="form-control" id="floatingInput" placeholder="Inserisci il nome" name="ripetiPassword" required>
+      <label for="floatingInput">Ripeti password</label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit" name="SignIn">Registrati</button>
+    </form>
+</main>
+
+
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container text-center">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+      
+  </body>
+</html>
