1e5c73844cef43952b5f00e3d6552f04fabe13a6
vinz1995 <83903266+vinz1995@users.noreply.github.com>
2021-07-09 16:41:33 +0200

    vincenzo
    up title

--- segnalazione.php
+++ segnalazione.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['loggedin'])) {
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.83.1">
-    <title>Sticky Footer Navbar Template · Bootstrap v5.0</title>
+    <title>Segnalazione</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   	<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
 
@@ -37,7 +37,7 @@ if (!isset($_SESSION['loggedin'])) {
       }
     </style>
     
-    <title>OpenLayers example</title>
+
   </head>
   <body class="d-flex flex-column h-100">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
