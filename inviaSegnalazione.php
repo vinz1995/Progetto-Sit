056229e3bd6e3cd4d9aca910f85bba12b03e3186
vinz1995 <83903266+vinz1995@users.noreply.github.com>
2021-07-09 17:25:20 +0200

    vin
    add form

---
+++ inviaSegnalazione.php
@@ -0,0 +1,7 @@
+<?php
+echo $_POST['lat'];
+echo $_POST['lon'];
+echo $_POST['dimensioneBuca'];
+echo $_POST['descrizione'];
+
+?>
--- segnalazione.php
+++ segnalazione.php
@@ -7,112 +7,117 @@ if (!isset($_SESSION['loggedin'])) {
     exit;
 }
 ?>
-
 <!doctype html>
 <html lang="en" class="h-100">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.83.1">
     <title>Segnalazione</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  	<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
-
-
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
     <!-- Bootstrap core CSS -->
-
-
     <!-- Custom styles for this template -->
     <link href="css/sticky-footer-navbar.css" rel="stylesheet">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css">
     <!-- <script src="https://unpkg.com/elm-pep"></script> -->
     <style>
-      .map {
+        .map {
         height: 400px;
         width: 100%;
       }
     </style>
-    
+</head>
 
-  </head>
-  <body class="d-flex flex-column h-100">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
-<header class="p-2 bg-dark text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        </a>
-        <ul class="nav col-12 col-lg-auto col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="home.php" class="nav-link px-1 text-white">Home</a></li>
-          <li><a href="segnalazione.php" class="nav-link px-1 text-white">Segnalazione</a></li>
-          <li><a href="visualizzaSegnalazione.php" class="nav-link px-1 text-white">Visualizza Segnalazione</a></li>
-        </ul>
-
-        <div class="text-end">
-          <ul class="nav col-12 col-lg-auto me-lg-auto mb-1 justify-content-center mb-md-0">
-            <li> <a href="profilo.php" class="nav-link px-2 text-white"><?php echo $_SESSION['nome'].' '.$_SESSION['cognome'];?></a></li>
-           <li> <a class="btn btn-outline-light me-2" href="logout.php" role="button">Esci</a></li>
-          </ul>
+<body class="d-flex flex-column h-100">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+    <header class="p-2 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap" /></svg>
+                </a>
+                <ul class="nav col-12 col-lg-auto col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="home.php" class="nav-link px-1 text-white">Home</a></li>
+                    <li><a href="segnalazione.php" class="nav-link px-1 text-white">Segnalazione</a></li>
+                    <li><a href="visualizzaSegnalazione.php" class="nav-link px-1 text-white">Visualizza Segnalazione</a></li>
+                </ul>
+                <div class="text-end">
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-1 justify-content-center mb-md-0">
+                        <li> <a href="profilo.php" class="nav-link px-2 text-white">
+                                <?php echo $_SESSION['nome'].' '.$_SESSION['cognome'];?></a></li>
+                        <li> <a class="btn btn-outline-light me-2" href="logout.php" role="button">Esci</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-</header>
+    </header>
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+        <div class="container">
+            <div class="container mt-auto">
+                <div class="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-1">
+                    <div class="col mt-1">
+                        <h3>utente</h3>
+                        <!--  echo $_COOKIE['profile_viewer_uid'];  -->
+                        <form method="POST" action="inviaSegnalazione.php">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="lat" name="lat">
+                                <label for="floatingPassword">lat</label>
+                            </div>
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="lon" name="lon">
+                                <label for="floatingPassword">lon</label>
+                            </div>
+                            <div class="form-floating">
+                                <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="dimensioneBuca">
+                                    <option selected value="piccola">Piccola</option>
+                                    <option value="media">Media</option>
+                                    <option value="grande">Grande</option>
+                                </select>
+                                <label for="floatingSelect">Dimensione buca</label>
+                            </div>
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="descrizione"></textarea>
+                                <label for="floatingTextarea2">Descrizione</label>
+                            </div>
+                            <div class="mb-3">
+                                <input class="form-control form-control-sm" id="formFileSm" type="file">
+                            </div>
+                            <div>
+                                <button class="w-100 btn btn-lg btn-primary" type="submit" name="inviaSegnalazione">Invia</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col mt-1">
+                        <div id="map" class="map"></div>
+                        <div id="info" style="display: none;"></div>
+                        <label for="track">
+                            track position
+                            <input id="track" type="checkbox" />
+                        </label>
+                        <p>
+                            position accuracy : <code id="accuracy"></code>&nbsp;&nbsp;
+                            <!-- altitude accuracy : <code id="altitudeAccuracy"></code>&nbsp;&nbsp;
+                            heading : <code id="heading"></code>&nbsp;&nbsp;
+                            speed : <code id="speed"></code> -->
+                        </p>
+                        <script src="mappaConPosizione.js"></script>
+                    </div>
+                </div>
+            </div>
+    </main>
+    <footer class="footer mt-auto py-3 bg-dark">
+        <div class="container text-center">
+            <span class="text-white f-ce">Place sticky footer content here.</span>
+        </div>
+        <h1 class="visually-hidden">Sidebars examples</h1>
+    </footer>
+</body>
 
-<!-- Begin page content -->
-<main class="flex-shrink-0">
-  <div class="container">
-  	<div class="container mt-auto">
-  <div class="row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-1">
-    <div class="col mt-1">
-      <h3>utente</h3>
-     <!--  echo $_COOKIE['profile_viewer_uid'];  -->
-
-      <form>
-      <div class="form-floating">
-      <input type="text" class="form-control" id="lat" >
-      <label for="floatingPassword">lat</label>
-    </div>
-    <div class="form-floating">
-      <input type="text" class="form-control" id="lon" >
-      <label for="floatingPassword">lon</label>
-    </div>
-      </form>
-    </div>
-    <div class="col mt-1">
-    <div id="map" class="map"></div>
-    <div id="info" style="display: none;"></div>
-    <label for="track">
-      track position
-      <input id="track" type="checkbox"/>
-    </label>
-    <p>
-      position accuracy : <code id="accuracy"></code>&nbsp;&nbsp;
-      <!-- altitude accuracy : <code id="altitudeAccuracy"></code>&nbsp;&nbsp;
-      heading : <code id="heading"></code>&nbsp;&nbsp;
-      speed : <code id="speed"></code> -->
-    </p>
-    <script src="mappaConPosizione.js"></script>
-    
-    </div>
-    
-  </div>
-</div>
-</main>
-
-<footer class="footer mt-auto py-3 bg-dark">
-  <div class="container text-center">
-    <span class="text-white f-ce" >Place sticky footer content here.</span>
-  </div>
-   <h1 class="visually-hidden">Sidebars examples</h1>
-
-
-</footer>
-
-  </body>
 </html>
