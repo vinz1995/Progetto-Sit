a1b7ed8b377733147fcc9b5fbb78a0d3414b40e3
vinz1995 <83903266+vinz1995@users.noreply.github.com>
2021-07-27 16:14:31 +0200

    ok

--- openJ/Start/src/sit/graphReduce.java
+++ openJ/Start/src/sit/graphReduce.java
@@ -132,7 +132,7 @@ public class graphReduce extends AbstractPlugIn {
 		Layer layer_strade=s.get(1);
 		Layer layer_segnalazioni=s.get(2);
 		VisBuffer(layer_rimessa.getFeatureCollectionWrapper().getFeatures().get(0).getGeometry(), distanzaMassima(arg0, layer_rimessa, layer_segnalazioni), arg0);
-		addPointStrade(arg0, ResSpatialQ(layer_rimessa.getFeatureCollectionWrapper().getFeatures().get(0).getGeometry(), layer_strade, arg0, distanzaMassima(arg0, layer_rimessa, layer_segnalazioni)));
+		// addPointStrade(arg0, ResSpatialQ(layer_rimessa.getFeatureCollectionWrapper().getFeatures().get(0).getGeometry(), layer_strade, arg0, distanzaMassima(arg0, layer_rimessa, layer_segnalazioni)));
 		return false;
 	}
     
